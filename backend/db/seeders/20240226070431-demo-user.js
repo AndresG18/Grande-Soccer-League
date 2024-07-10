3e6e9ceb2437740a530a168a2123b72233629201
Andres Garcia <Andrese17g@gmail.com>
2024-07-10 15:38:58 -0700

    added user seeders

--- backend/db/seeders/20240226070431-demo-user.js
+++ backend/db/seeders/20240226070431-demo-user.js
@@ -2,7 +2,7 @@
 
 const { User } = require('../models');
 const bcrypt = require("bcryptjs");
-const {Op} = require('sequelize')
+const { Op } = require('sequelize');
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
@@ -13,34 +13,45 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await User.bulkCreate([
       {
-        firstName: "Demo",
-        lastName: "User",
+        first_name: "Demo",
+        last_name: "User",
         email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        phone: '123-456-7890',
+        hashedPassword: bcrypt.hashSync('newpassword1'),
+        type: 'admin',
+        goals: 0,
+        assists: 0,
+        approved: true
       }, {
-        firstName: "Darth",
-        lastName: "Cina",
+        first_name: "Daniel",
+        last_name: "Garcia",
         email: 'demo1@user.io',
-        username: 'DarthCiina',
-        hashedPassword: bcrypt.hashSync('password2')
+        phone: '123-456-7891',
+        hashedPassword: bcrypt.hashSync('newpassword2'),
+        type: 'admin',
+        goals: 0,
+        assists: 0,
+        approved: true
       }, {
-        firstName: "Andres",
-        lastName: "Garcia",
+        first_name: "Andres",
+        last_name: "Garcia",
         email: 'demo2@user.io',
-        username: 'Andres',
-        hashedPassword: bcrypt.hashSync('password3')
+        phone: '123-456-7892',
+        hashedPassword: bcrypt.hashSync('password'),
+        type: 'admin',
+        goals: 0,
+        assists: 0,
+        approved: true
       }
     ], { validate: true });
-
   },
 
   async down(queryInterface, Sequelize) {
-  options.tableName = 'Users'
-    await queryInterface.bulkDelete(options,{
-        username:{
-        [Op.in] : ["Demo", 'Dulcina', 'Andres']
+    options.tableName = 'Users';
+    await queryInterface.bulkDelete(options, {
+      email: {
+        [Op.in]: ['demo@user.io', 'demo1@user.io', 'demo2@user.io']
       }
-    })
+    });
   }
 };
