69aca639e81b654fe2acfc1ad52694e0e9c738b2
Andres Garcia <Andrese17g@gmail.com>
2024-07-11 20:50:17 -0700

    added home

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,13 +1,13 @@
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton-bonus';
 import './Navigation.css';
-import '../../../public/Logo.png'
+// import '../../../public/Logo.png'
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
     <div className='navBar'>
-     <img src='http://pinfluence-2024.s3.amazonaws.com/12467bab7ad243e3b498a0ecdbb8cd2b.png' alt="" style={{height:'50px',width:'200px',marginLeft:'5%'}} />
+     <img src='../../../src/Logo.png' alt="" style={{height:'50px',width:'200px',marginLeft:'5%'}} />
       {isLoaded && <ProfileButton user={sessionUser} />}
     </div>
   );
