2cb63dce4d37749ba5f4e450f0272e45987d8021
Andres Garcia <Andrese17g@gmail.com>
2024-07-11 20:27:18 -0700

    added home

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ function Navigation({ isLoaded }) {
 
   return (
     <div className='navBar'>
-     <img src='../../../public/Logo.png' alt="" style={{height:'50px',width:'200px',marginLeft:'5%'}} />
+     <img src='http://pinfluence-2024.s3.amazonaws.com/12467bab7ad243e3b498a0ecdbb8cd2b.png' alt="" style={{height:'50px',width:'200px',marginLeft:'5%'}} />
       {isLoaded && <ProfileButton user={sessionUser} />}
     </div>
   );
